3ceeb3902d3f09a283184a086c72a245e896708b
Márcio Simão <marciosimao0825@gmail.com>
2026-05-10 15:02:57 +0100

    criando menu

--- Keba/src/keba/Keba.java
+++ Keba/src/keba/Keba.java
@@ -16,7 +16,53 @@ public class Keba {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner input = new Scanner(System.in);
-        int opcao;
+        int opcao = 0;
+        
+        do 
+        {
+            System.out.println("==============================================");
+            System.out.println("KEBA - SCANNER DE PHISHING");             
+            System.out.println("Menu Principal");                        
+            System.out.println("==============================================");
+            System.out.println("1. Analisar um ficheiro de emails");          
+            System.out.println("2. Ver histórico de análises");              
+            System.out.println("3. Gerir lista de palavras suspeitas");       
+            System.out.println("4. Gerir lista negra de domínios");           
+            System.out.println("5. Gerar relatório (exportar para ficheiro)");
+            System.out.println("6. Ver estatísticas gerais");                 
+            System.out.println("0. Sair");                                    
+            System.out.println("==============================================");
+            System.out.print("Escolha uma opção: ");   
+            opcao = input.nextInt();
+
+            switch(opcao)
+            {
+                case 1:
+                
+                break;
+                case 2:
+                
+                break;
+                case 3:
+                
+                break;
+                case 4:
+                
+                break;
+                case 5:
+                
+                break;
+                case 6:
+                
+                break;
+                case 0:
+                
+                break;
+                default:
+                    System.out.println("Valor Invalido");
+                break;
+            }
+        }while(opcao == 0);
         
     }
     
