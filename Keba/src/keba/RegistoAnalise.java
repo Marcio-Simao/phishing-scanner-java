29a48f7931c06601c4ce14af19f0ea51bd81b1f4
Márcio Simão <marciosimao0825@gmail.com>
2026-06-28 20:04:48 +0100

    Update RegistoAnalise.java

--- Keba/src/keba/RegistoAnalise.java
+++ Keba/src/keba/RegistoAnalise.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package keba;
 
-/**
- *
- * @author Márcio Simão
- */
 public class RegistoAnalise {
     
+    private String dataAnalise;
+    private String ficheiroAnalisado;
+    private int totalEmails;
+    private int emailsSuspeitos;
+    private int emailsSeguros;
+    
+    // Construtor
+    public RegistoAnalise(String dataAnalise, String ficheiroAnalisado, 
+                          int totalEmails, int emailsSuspeitos, int emailsSeguros) {
+        this.dataAnalise = dataAnalise;
+        this.ficheiroAnalisado = ficheiroAnalisado;
+        this.totalEmails = totalEmails;
+        this.emailsSuspeitos = emailsSuspeitos;
+        this.emailsSeguros = emailsSeguros;
+    }
+    
+    // Getters
+    public String getDataAnalise() { return dataAnalise; }
+    public String getFicheiroAnalisado() { return ficheiroAnalisado; }
+    public int getTotalEmails() { return totalEmails; }
+    public int getEmailsSuspeitos() { return emailsSuspeitos; }
+    public int getEmailsSeguros() { return emailsSeguros; }
+    
+    // Setters
+    public void setDataAnalise(String dataAnalise) { this.dataAnalise = dataAnalise; }
+    public void setFicheiroAnalisado(String f) { this.ficheiroAnalisado = f; }
+    public void setTotalEmails(int t) { this.totalEmails = t; }
+    public void setEmailsSuspeitos(int e) { this.emailsSuspeitos = e; }
+    public void setEmailsSeguros(int e) { this.emailsSeguros = e; }
+    
+    @Override
+    public String toString() {
+        return "Data: " + dataAnalise + 
+               " | Ficheiro: " + ficheiroAnalisado +
+               " | Total: " + totalEmails +
+               " | Suspeitos: " + emailsSuspeitos +
+               " | Seguros: " + emailsSeguros;
+    }
 }
